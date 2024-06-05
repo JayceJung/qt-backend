a01941bfd0ff0ded1ee33abc7e5cfc6239ceb16e
YoungSuChae <codudtn369@gmail.com>
2024-06-04 22:12:51 -0400

    Routes for logout

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,7 +1,12 @@
-import express from 'express';
-import { loginUser, registerUser } from '../controllers/authController';
+import express from "express";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/authController";
 
 export const authRoutes = express.Router();
 
-authRoutes.post('/login', loginUser);
-authRoutes.post('/register', registerUser);
+authRoutes.post("/login", loginUser);
+authRoutes.post("/register", registerUser);
+authRoutes.post("/logout", logoutUser);
